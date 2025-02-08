60f531bb570d9d7cf5fe8c1bdbd893cff504aab7
Daniel <jugando.con.cpp@gmail.com>
2025-02-07 17:33:15 -0800

    Copiar los contenidos de episodios 1, 2 y 3.

---
+++ Episodio002/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+extern std::string hello;
+
+int main()
+{
+     std::cout << hello << std::endl;
+}
---
+++ Episodio003/hello.h
@@ -0,0 +1,8 @@
+#ifndef HELLO_H__INCLUDED
+#define HELLO_H__INCLUDED
+
+#include <string>
+
+extern std::string const hello;
+
+#endif  // HELLO_H__INCLUDED
