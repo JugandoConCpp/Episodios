b481e8d209704b14067517ff9865ea2bd81aff40
Daniel <jugando.con.cpp@gmail.com>
2025-02-19 17:45:36 -0800

    RIFF

---
+++ RIFF/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "riff.h"
+
+int main(int argc, char* argv[])
+{
+     if (argc != 2)
+     {
+          std::cout << "Falta proporcionar el nombre del archivo." << std::endl;
+     }
+
+     try {
+          RIFF riff(argv[1]);
+     } catch (RIFF::Chunk::Type type) {
+          std::cout << "Exception!  " << type << std::endl;
+     } catch (size_t size) {
+          std::cout << "Size exception!  " << size << std::endl;
+     } catch (...) {
+          std::cout << "Unknown exception!  " << std::endl;
+     }
+
+     return 0;
+}
